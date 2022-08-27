13a710a7ca88679f7e8a8ae4fc8d102fa841b3c6
Emir Savranogullari <emirsavranogullari@gmail.com>
2022-08-27 12:51:28 +0300

    Configure Vite build as library
    * create widget.html to test build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.tsx"),
+      name: "MyLib",
+      // the proper extensions will be added
+      fileName: "my-lib",
+    },
+  },
 });
