9566bd1a0aaecacc6c27cff5a5a9242b062ebf93
mertdeveci5 <mertdeveci5@gmail.com>
2022-08-29 14:06:43 +0100

    crypto auth integrated

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,29 @@
 import { useEffect, useState } from "preact/hooks";
 import "./app.css";
+import Profile from "./Profile";
+import { WagmiConfig, createClient } from "wagmi";
+import { getDefaultProvider } from "ethers";
 
 window.app = {};
 
 export function App() {
   const [name, setName] = useState("");
+  const client = createClient({
+    autoConnect: true,
+    provider: getDefaultProvider(),
+  });
 
   useEffect(() => {
     window.app.setName = setName;
     return () => (window.app.setName = undefined);
   }, []);
 
-  return <h1>Hello {name}</h1>;
+  return (
+    <>
+      <WagmiConfig client={client}>
+        <h1>Hello {name}</h1>
+        <Profile />
+      </WagmiConfig>
+    </>
+  );
 }
