e14516323826b04185238e1460a34d5c12e47d04
Emir Savranogullari <emirsavranogullari@gmail.com>
2022-08-31 00:35:34 +0300

    Add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), visualizer()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.tsx"),
